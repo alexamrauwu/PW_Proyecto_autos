4aca198ab3730225c2e31c3e0c9333b787d16f83
alexamrauwu <L181050165@pabellon.tecnm.mx>
2021-05-25 15:11:56 -0500

    f

---
+++ estacionamiento/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from estacionamiento.models import *
+
+# Register your models here.
+
+admin.site.register(autos)
+admin.site.register(administrador)
+admin.site.register(sistema)
+admin.site.register(personas)
